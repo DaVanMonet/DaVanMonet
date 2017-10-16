ba2e735ded95d1bca98872a4a811dda140500e6b
Wezz Balk <wezz@wezz.se>
2017-10-16 13:24:05 +0200

    Click on ID Label will now select the the id

--- preview/vuecomponents/Maincontent.vue
+++ preview/vuecomponents/Maincontent.vue
@@ -4,15 +4,15 @@
     <div class="davanmonet-maincontent-preamble" v-if="content.Preamble" v-html="content.Preamble"></div>
     <div class="davanmonet-maincontent-mainbody" v-if="content.MainBody" v-html="content.MainBody"></div>
     <div v-if="content.ComponentItems" class="davanmonet-maincontent-sections">
-      <div v-for="componentItem in content.ComponentItems" class="davanmonet-maincontent-section">
+      <div v-for="(componentItem, index) in content.ComponentItems" class="davanmonet-maincontent-section">
        
         <component-showcase :css-breakpoints="cssBreakpoints" :showcase-data="componentItem"></component-showcase>
-
-        <div v-if="componentItem.id" class="davanmonet-maincontent-section-id">{{componentItem.id}}</div>
-
+      
+        <div v-if="componentItem.id" class="davanmonet-idtext"><label :for="'componentid-' + index" v-on:click.stop="ClickOnIDLabel('componentid-' + index)">Component ID:</label> 
+        <input type="text" :id="'componentid-' + index" :value="componentItem.id"/></div>
       </div>
     </div>
-    <div v-if="content.id" class="davanmonet-maincontent-id">{{content.id}}</div>
+    <div v-if="content.id" class="davanmonet-idtext" ><label for="articleid" v-on:click.stop="ClickOnIDLabel('articleid')">Article ID:</label> <input type="text" id="articleid" :value="content.id"/></div>
   </div>
 </template>
 
@@ -30,6 +30,12 @@ module.exports = {
   [
     "url:/vuecomponents/component/ComponentShowcase.vue"
   ],
-  props: ['content','cssBreakpoints']
+  props: ['content','cssBreakpoints'],
+  methods: {
+    ClickOnIDLabel(inputid)
+    {
+      let input = document.getElementById(inputid);
+    }
+  }
 };
 </script>
