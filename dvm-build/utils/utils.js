abfe7ffc1ddf25958ddf918dae0ee6043a53b576
Joacim Magnusson <splashdust123@gmail.com>
2017-11-20 11:43:42 +0100

    * Fixed build paths when used as a package

--- dvm-build/utils/utils.js
+++ dvm-build/utils/utils.js
@@ -6,6 +6,13 @@ exports.assetsPath = function (_path) {
   var assetsSubDirectory = process.env.NODE_ENV === 'production'
     ? config.build.assetsSubDirectory
     : config.dev.assetsSubDirectory
+  return path.resolve(process.cwd(), path.posix.join(assetsSubDirectory, _path))
+}
+
+exports.relativeAssetsPath = function (_path) {
+  var assetsSubDirectory = process.env.NODE_ENV === 'production'
+    ? config.build.assetsSubDirectory
+    : config.dev.assetsSubDirectory
   return path.posix.join(assetsSubDirectory, _path)
 }
 
--- dvm-build/webpack/webpack.dvm.prod.conf.js
+++ dvm-build/webpack/webpack.dvm.prod.conf.js
@@ -28,8 +28,8 @@ var webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    filename: utils.relativeAssetsPath('js/[name].[chunkhash].js'),
+    chunkFilename: utils.relativeAssetsPath('js/[id].[chunkhash].js')
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
@@ -47,7 +47,7 @@ var webpackConfig = merge(baseWebpackConfig, {
 
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css')
+      filename: utils.relativeAssetsPath('css/[name].[contenthash].css')
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
@@ -63,7 +63,7 @@ var webpackConfig = merge(baseWebpackConfig, {
       filename: process.env.NODE_ENV === 'testing'
         ? 'index.html'
         : config.build.index,
-      template: 'dvm-app/index.html',
+      template: path.resolve(__dirname, '../../dvm-app/index.html'),
       inject: true,
       minify: {
         removeComments: true,
