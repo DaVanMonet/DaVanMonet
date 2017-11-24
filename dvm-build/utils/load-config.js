cc258242351f7baa8e93428a742f322060cdb831
Joacim Magnusson <splashdust123@gmail.com>
2017-11-24 16:12:37 +0100

    * Add _abs entries in config that can be used whn absolute path is needed, thus eliminating the need of having path.resolve() in lots of places * Added a fix for when user config is _NOT_ used by a user

--- dvm-build/utils/load-config.js
+++ dvm-build/utils/load-config.js
@@ -2,6 +2,7 @@
  * @file Load config files and cread the config object for DvM
  */
 
+const path = require('path');
 const yaml = require('js-yaml');
 const fs = require('fs');
 const _ = require('lodash');
@@ -10,6 +11,19 @@ const globby = require('globby');
 const isType = (val, type) => (typeof val === type && (type !== "string" || (type === "string" && val.length > 0)));
 const getDirs = p => fs.readdirSync(p).filter(f => fs.statSync(p+"/"+f).isDirectory());
 const loadConfigFile = path => (path.indexOf('.json') !== -1) ? require(path) : yaml.safeLoad(fs.readFileSync(path, 'utf8'));
+
+// Resolve path or set to empty string, so checks aren't needed elsewhere
+function resolvePath(p)
+{
+    if (isType(p, "string") && p.length > 0)
+    {
+        return path.resolve(process.cwd(), p)
+    }
+    else
+    {
+        return "";
+    }
+}
 
 let cachedConfig = null;
 
@@ -22,24 +36,38 @@ module.exports = function()
     else
     {
         // Set path to the configFile set in package.json, or use deafult path
-        var defaultPath = process.cwd() + "/dvm-config/projectoptions.yml";
-        var path = process.cwd() + '/' + process.env.npm_package_config_configFile || defaultPath;
+        var default_path = process.cwd() + "/dvm-config/projectoptions.yml";
+        var config_path = process.cwd() + '/' + process.env.npm_package_config_configFile || default_path;
 
-        if (fs.existsSync(path))
+        if (fs.existsSync(config_path))
         {
             // Load JSON or YAML base confg file
-            var config = loadConfigFile(path);
+            var config = loadConfigFile(config_path);
 
-            // Make sure certain entries exist as strings, so we don't need to check all the time in other places
-            if (isType(config.userconfig, "string") === false)
-            {
-                config.userconfig = "";
-            }
+            // Resolve some paths so we can have cleaner code elsewhere
+            config.userconfig_abs = resolvePath(config.userconfig);
+            config.directories.src_abs = resolvePath(config.directories.src);
+            config.directories.build_abs = resolvePath(config.directories.build);
+            config.directories.dist_web_abs = resolvePath(config.directories.dist_web);
+            config.directories.dist_package_abs = resolvePath(config.directories.dist_package);
+            config.directories.indexes_abs = resolvePath(config.directories.indexes);
+            config.directories.configs_abs = resolvePath(config.directories.configs);
+            config.directories.cssCopies_abs = resolvePath(config.directories.cssCopies);
             
+            // Special check for user config.
+            // Should default to user config file from package if not specified locally.
+            // This is needed because the file is bundled at compile-time and needs to exist.
+            // Moste likely this could have a nicer solution, but this will do for now.
+            if(config.userconfig_abs === "")
+            {
+                // Hard coded path, because end user should not be able to override any of this
+                config.userconfig_abs = path.resolve(__dirname, '../../configs/local-conf.json');
+            }
+
             // If we have a user config file configured
-            if (config.userconfig.length > 0) {
+            if (config.userconfig_abs.length > 0) {
                 // Load JSON or YAML user config file
-                var config_user = loadConfigFile(process.cwd() + '/' + config.userconfig);
+                var config_user = loadConfigFile(config.userconfig_abs);
                 
                 // Merge to a single config
                 var config = _.merge(config, config_user);
@@ -61,8 +89,8 @@ module.exports = function()
             {
                 // Glob all the globs
                 let e = globby.sync(config.compilation.targets[target_name],
-                    { cwd: process.cwd() + '/' + config.directories.src })
-                    .map(relPath => process.cwd() + '/' + config.directories.src + '/' + relPath); // Fix relative paths
+                    { cwd: config.directories.src_abs })
+                    .map(relPath => config.directories.src_abs + '/' + relPath); // Fix relative paths
 
                 config.compilation.entry[target_name] = e;
             }
@@ -73,7 +101,7 @@ module.exports = function()
         }
         else
         {
-            console.error('File does not exist ('+ path +')');
+            console.error('File does not exist ('+ config_path +')');
         }
     }
 }
