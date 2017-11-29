fb9a613473b7d0b23da24446eb66d41ae60fce0d
Joacim Magnusson <splashdust123@gmail.com>
2017-11-29 10:40:19 +0100

    * Fixed IE regression

--- dvm-app/DaVanMonet.vue
+++ dvm-app/DaVanMonet.vue
@@ -109,7 +109,7 @@ export default {
 				//this.$nextTick(() =>
 				//{
 				//	afterRender(path);
-				//});	
+				//});
 			}
 		},
 		
--- dvm-app/src/main.js
+++ dvm-app/src/main.js
@@ -1,4 +1,7 @@
+import 'babel-polyfill';
+
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 import DaVanMonet from '@/DaVanMonet.vue';
 
@@ -7,11 +10,25 @@ if ( ! NodeList.prototype.forEach ) {
 	NodeList.prototype.forEach = Array.prototype.forEach;
 }
 
+// const Foo = { template: '<div>foo</div>' }
+// const Bar = { template: '<div>bar</div>' }
+
+// const routes = [
+// 	{ path: '/foo', component: Foo },
+// 	{ path: '/bar', component: Bar }
+// ]
+
+// const router = new VueRouter({
+// 	routes
+// })
+
 new Vue({
 	el: '#davanmonet-app',
 	template: '<DaVanMonet ref="dvm" />',
+	//router,
 	components: { DaVanMonet },
 	methods: {
+		
 		// TODO: This should problaby be handled by a router instead
 		async loadPage(_vue, path) {
 			return this.$refs.dvm.loadPage(_vue, path);
@@ -31,5 +48,5 @@ new Vue({
 		getConfig() {
 			return this.$refs.dvm.projectConfig;
 		}
-	}	
+	}
 });
