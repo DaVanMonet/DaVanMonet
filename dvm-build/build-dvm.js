141b54dccf429280306707d13d4f6eb2b6db20d4
Wezz Balk <wezz@wezz.se>
2017-12-04 10:39:11 +0100

    Package build now cleans it's output folder before running

--- dvm-build/build-dvm.js
+++ dvm-build/build-dvm.js
@@ -13,48 +13,41 @@ var webpackConfig = require('./webpack/webpack.dvm.prod.conf')
 var spinner = ora('building for production...')
 spinner.start()
 
-// Start legacy grunt build
-// var sys = require('sys')
-// var exec = require('child_process').exec;
-// var child;
-// child = exec("grunt build", function (error, stdout, stderr) {
-//   sys.print('stdout: ' + stdout);
-//   sys.print('stderr: ' + stderr);
-//   if (error !== null) {
-//     console.log('grunt dev error: ' + error);
-//   }
-// });
+rm(path.join(envConfig.build.assetsRoot, envConfig.build.assetsSubDirectory), err =>
+{
+	if (err) throw err
+	webpack(webpackConfig, function(err, stats)
+	{
+		spinner.stop()
+		if (err) throw err
+		process.stdout.write(stats.toString(
+		{
+			colors: true,
+			modules: false,
+			children: false,
+			chunks: false,
+			chunkModules: false
+		}) + '\n\n')
 
-rm(path.join(envConfig.build.assetsRoot, envConfig.build.assetsSubDirectory), err => {
-  if (err) throw err
-  webpack(webpackConfig, function (err, stats) {
-    spinner.stop()
-    if (err) throw err
-    process.stdout.write(stats.toString({
-      colors: true,
-      modules: false,
-      children: false,
-      chunks: false,
-      chunkModules: false
-    }) + '\n\n')
+		console.log(stats.toString(
+		{
+			colors: true,
+			modules: false,
+			children: false,
+			chunks: false,
+			chunkModules: false
+		}));
 
-    console.log(stats.toString({
-      colors: true,
-      modules: false,
-      children: false,
-      chunks: false,
-      chunkModules: false
-    }));
+		if (stats.hasErrors())
+		{
+			console.log(chalk.red('  Build failed with errors.\n'))
+			process.exit(1)
+		}
 
-    if (stats.hasErrors()) {
-      console.log(chalk.red('  Build failed with errors.\n'))
-      process.exit(1)
-    }
-
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
-  })
+		console.log(chalk.cyan('  Build complete.\n'))
+		console.log(chalk.yellow(
+			'  Tip: built files are meant to be served over an HTTP server.\n' +
+			'  Opening index.html over file:// won\'t work.\n'
+		))
+	})
 })
--- dvm-build/build-patternlibrary.js
+++ dvm-build/build-patternlibrary.js
@@ -3,6 +3,7 @@
  */
 
 const path = require('path')
+var rm = require('rimraf')
 const chalk = require('chalk')
 const webpack = require('webpack')
 const merge = require('webpack-merge');
@@ -15,36 +16,41 @@ const webpackConfig = merge(webpackProdConf, webpackConfigProjectPL)
 // Load config
 const dvm_config = require('./utils/load-config').dvmConfig();
 
-// Generate fresh contentindex.json
-require('./utils/create-content-index')(dvm_config);
-
-webpack(webpackConfig, function (err, stats) {
-    //spinner.stop()
+rm(path.join(dvm_config.directories.dist_package), err =>
+{
     if (err) throw err
-    process.stdout.write(stats.toString({
-        colors: true,
-        modules: false,
-        children: false,
-        chunks: false,
-        chunkModules: false
-    }) + '\n\n')
 
-    console.log(stats.toString({
-        colors: true,
-        modules: false,
-        children: false,
-        chunks: false,
-        chunkModules: false
-    }));
+    // Generate fresh contentindex.json
+    require('./utils/create-content-index')(dvm_config);
 
-    if (stats.hasErrors()) {
-        console.log(chalk.red('  Build failed with errors.\n'))
-        process.exit(1)
-    }
+    webpack(webpackConfig, function (err, stats) {
+        //spinner.stop()
+        if (err) throw err
+        process.stdout.write(stats.toString({
+            colors: true,
+            modules: false,
+            children: false,
+            chunks: false,
+            chunkModules: false
+        }) + '\n\n')
 
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-        '  Tip: built files are meant to be served over an HTTP server.\n' +
-        '  Opening index.html over file:// won\'t work.\n'
-    ))
+        console.log(stats.toString({
+            colors: true,
+            modules: false,
+            children: false,
+            chunks: false,
+            chunkModules: false
+        }));
+
+        if (stats.hasErrors()) {
+            console.log(chalk.red('  Build failed with errors.\n'))
+            process.exit(1)
+        }
+
+        console.log(chalk.cyan('  Build complete.\n'))
+        console.log(chalk.yellow(
+            '  Tip: built files are meant to be served over an HTTP server.\n' +
+            '  Opening index.html over file:// won\'t work.\n'
+        ))
+    });
 });
