768232cf1cc214f52446f9966023a3202dbd2278
Joacim Magnusson <joacim.magnusson@seb.se>
2019-11-26 16:43:08 +0100

    feat: upgrade to babel 7

--- dvm-build/webpack/webpack.patternlibrary.base.conf.js
+++ dvm-build/webpack/webpack.patternlibrary.base.conf.js
@@ -1,125 +1,123 @@
 /**
  * @file Webpack base config for pattern libraries
- * 
+ *
  */
 
-const path = require('path')
-const utils = require('../utils/utils')
-const glob = require("glob")
-const webpack = require('webpack');
-const fs = require('fs-extra')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require("path");
+const utils = require("../utils/utils");
+const glob = require("glob");
+const webpack = require("webpack");
+const fs = require("fs-extra");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const vueLoaderConfig = require('./vue-loader.conf')
+const vueLoaderConfig = require("./vue-loader.conf");
 
-const LifecyclePlugin = require('../plugins/webpack-lifecycle-plugin');
-const ContentIndexResolver = require('../plugins/content-index-resolver');
+const LifecyclePlugin = require("../plugins/webpack-lifecycle-plugin");
+const ContentIndexResolver = require("../plugins/content-index-resolver");
 
-const dvmConfig = require('../utils/load-config').dvmConfig();
+const dvmConfig = require("../utils/load-config").dvmConfig();
 
-const
-{
-	additionalRules,
-	additionalPlugins
-} = require('../utils/cssfile-rulegenerator')();
+const {
+  additionalRules,
+  additionalPlugins
+} = require("../utils/cssfile-rulegenerator")();
 
 module.exports = {
-	name: "patternlibrary",
+  name: "patternlibrary",
 
-	entry:
-	{
-		...dvmConfig.compilation.entry,
-		'preview-frame.js': path.resolve(__dirname, '../../dvm-app/src/preview-frame.js')
-	},
+  entry: {
+    ...dvmConfig.compilation.entry,
+    "preview-frame.js": path.resolve(
+      __dirname,
+      "../../dvm-app/src/preview-frame.js"
+    )
+  },
 
-	output:
-	{
-		path: dvmConfig.directories.dist_web_abs(),
-		filename: '[name].js',
-		publicPath: '/'
-	},
+  output: {
+    path: dvmConfig.directories.dist_web_abs(),
+    filename: "[name].js",
+    publicPath: "/"
+  },
 
-	resolve:
-	{
-		extensions: ['.ts', '.js', '.json', '.yml', '.ts'],
-		alias:
-		{
-			'vue$': 'vue/dist/vue.esm.js',
-			[dvmConfig.directories.configs]: dvmConfig.directories.configs_abs(),
-			[dvmConfig.directories.indexes]: dvmConfig.directories.indexes_abs()
-		},
-		plugins: [ContentIndexResolver]
-	},
+  resolve: {
+    extensions: [".ts", ".js", ".json", ".yml", ".ts"],
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
+      [dvmConfig.directories.configs]: dvmConfig.directories.configs_abs(),
+      [dvmConfig.directories.indexes]: dvmConfig.directories.indexes_abs()
+    },
+    plugins: [ContentIndexResolver]
+  },
 
-	plugins: [
-		new webpack.DefinePlugin(
-		{
-			__MAIN_CONFIG_PATH__: JSON.stringify(path.resolve(process.cwd(), process.env.npm_package_config_configFile)), // This is set in package.json //process.env.npm_package_config_configFile
-			__USER_CONFIG_PATH__: JSON.stringify(dvmConfig.userconfig_abs()),
-			__CONTENT_INDEX_PATH__: JSON.stringify(dvmConfig.directories.indexes_abs() + '/' + dvmConfig.indexing.contentIndexOutput),
-			__PACKAGE_JSON__: JSON.stringify(process.cwd() + "/package.json")
-		}),
+  plugins: [
+    new webpack.DefinePlugin({
+      __MAIN_CONFIG_PATH__: JSON.stringify(
+        path.resolve(process.cwd(), process.env.npm_package_config_configFile)
+      ), // This is set in package.json //process.env.npm_package_config_configFile
+      __USER_CONFIG_PATH__: JSON.stringify(dvmConfig.userconfig_abs()),
+      __CONTENT_INDEX_PATH__: JSON.stringify(
+        dvmConfig.directories.indexes_abs() +
+          "/" +
+          dvmConfig.indexing.contentIndexOutput
+      ),
+      __PACKAGE_JSON__: JSON.stringify(process.cwd() + "/package.json")
+    }),
 
-		// Emit CSS copies via LifeCycle plugin
-		new LifecyclePlugin(
-		{
-			"emit": (compilation, options, pluginOptions) =>
-			{
-				// Save css files to configuration directory
-				let cssDestinations = [];
-				if (dvmConfig.compilation.emitCssCopies === true)
-				{
-					cssDestinations.push(dvmConfig.directories.cssCopies)
-				}
-				require('../utils/emit-css-copies.js')(compilation.assets, cssDestinations);
-			}
-		}),
+    // Emit CSS copies via LifeCycle plugin
+    new LifecyclePlugin({
+      emit: (compilation, options, pluginOptions) => {
+        // Save css files to configuration directory
+        let cssDestinations = [];
+        if (dvmConfig.compilation.emitCssCopies === true) {
+          cssDestinations.push(dvmConfig.directories.cssCopies);
+        }
+        require("../utils/emit-css-copies.js")(
+          compilation.assets,
+          cssDestinations
+        );
+      }
+    }),
 
-		...additionalPlugins
+    ...additionalPlugins
+  ],
 
-	],
-
-	module:
-	{
-		rules: [
-			{
-				"test": /\.css$/,
-				"use":
-				[
-					"style-loader",
-					{
-						"loader": "css-loader",
-						"options":
-						{
-							"sourceMap": false,
-							"import": false
-						}
-					}
-				]
-			},
-			{
-				"test": /\.html$/,
-				"loader": "raw-loader"
-			},
-			{
-				test: /\.yml$/,
-				loader: 'yml-loader'
-			},
-			{
-                test: /\.jsx?$/,
-                exclude: /.*node_modules((?!davanmonet).)*$/,
-                loader: 'babel-loader',
-                query: {
-                    presets:[ 'stage-2' ]
-                }
-            },
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-				options: vueLoaderConfig
-			},
-			...additionalRules
-
-		]
-	}
-}
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: false,
+              import: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: "raw-loader"
+      },
+      {
+        test: /\.yml$/,
+        loader: "yml-loader"
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /.*node_modules((?!davanmonet).)*$/,
+        loader: "babel-loader",
+        query: {
+          presets: ["@babel/preset-env"]
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        options: vueLoaderConfig
+      },
+      ...additionalRules
+    ]
+  }
+};
